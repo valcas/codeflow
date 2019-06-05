1638082cc8b857d6da2e5dfdaf89d507d18ce786
Val Cassidy <val@codeumentus.com>
2019-06-05 13:19:59 +0100

    fixing sass conversion in electron build

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -12,6 +12,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.scss$/,
+        use: [
+              "style-loader", // creates style nodes from JS strings
+              "css-loader", // translates CSS into CommonJS
+              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
